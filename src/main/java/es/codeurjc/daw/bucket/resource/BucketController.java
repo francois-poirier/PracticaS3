6a3a3b9d8ec90d165eb73fe9f596455df6e24aa6
François <francois.poirier@bluetab.net>
2020-02-16 22:11:53 +0100

    path

--- src/main/java/es/codeurjc/daw/bucket/resource/BucketController.java
+++ src/main/java/es/codeurjc/daw/bucket/resource/BucketController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +22,6 @@ import es.codeurjc.daw.bucket.dto.BucketDto;
 import es.codeurjc.daw.bucket.service.BucketService;
 
 @RestController
-@RequestMapping("​/api/buckets")
 public class BucketController {
 
 	Logger logger = LoggerFactory.getLogger(BucketController.class);
@@ -35,34 +33,34 @@ public class BucketController {
 		this.bucketService = bucketService;
 	}
 
-	@GetMapping(path = "/")
+	@GetMapping(path = "​/api/buckets/")
 	public ResponseEntity<List<String>> listBuckets() {
 		return ResponseEntity.ok().body(bucketService.listBuckets());
 	}
 	
-	@GetMapping(path = "/{bucketName}")
+	@GetMapping(path = "​/api/buckets/{bucketName}")
     public ResponseEntity<BucketDto> getBucket(@PathVariable String bucketName) {
 		return ResponseEntity.ok().body(bucketService.getBucket(bucketName));
 	}
 	
-	@PostMapping(path = "/{bucketName}")
+	@PostMapping(path = "​/api/buckets/{bucketName}")
 	public ResponseEntity<BucketDto> createBucket(@PathVariable String bucketName) {
 		return new ResponseEntity<>(bucketService.createBucket(bucketName), HttpStatus.CREATED);
 	}
 	
-	@DeleteMapping(path = "/{bucketName}")
+	@DeleteMapping(path = "​/api/buckets/{bucketName}")
 	public ResponseEntity<Void> deleteBucket(@PathVariable String bucketName) {
 		bucketService.deleteBucket(bucketName);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
-	@DeleteMapping(path = "/{bucketName}/{objectName}")
+	@DeleteMapping(path = "​/api/buckets/{bucketName}/{objectName}")
 	public ResponseEntity<Void> deleteObject(@PathVariable String bucketName,@PathVariable String objectName) {
 		bucketService.deleteObject(bucketName,objectName);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
-	@PostMapping(path = "/{bucketName}/uploadObject")
+	@PostMapping(path = "​/api/buckets/{bucketName}/uploadObject")
 	public ResponseEntity<String> uploadFile(@PathVariable String bucketName,@RequestPart("file") MultipartFile multipartFile, @RequestPart("isPublic") Boolean isPublic) throws IOException {
 		String fileName = multipartFile.getOriginalFilename();
 		File file = new File("java.io.tmpdir"+"/"+fileName);
@@ -70,7 +68,7 @@ public class BucketController {
 		return new ResponseEntity<>(bucketService.uploadFile(bucketName,fileName,file,isPublic), HttpStatus.CREATED);              
 	}
 	
-	@PostMapping(path = "/{bucketName}/copyObject")
+	@PostMapping(path = "​/api/buckets/{bucketName}/copyObject")
 	public ResponseEntity<String> copyObject(@PathVariable String bucketName, @RequestParam("sourceKey") String sourceKey, @RequestParam("destinationBucketName") String destinationBucketName,
 			@RequestParam("destinationKey") String destinationKey) {
 		return new ResponseEntity<>(bucketService.copyObject(bucketName, sourceKey, destinationBucketName, destinationKey),HttpStatus.CREATED);
